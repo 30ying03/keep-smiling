221c7c495b852b684934beb0e52bb3d24d430c5d
30ying03 <52901857+30ying03@users.noreply.github.com>
2020-10-19 14:39:22 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,68 @@
+print("Title of program: Encouragement bot")
+print()
+while True:
+  description = input("Could you describe how you feel in a sentence?")
+
+  list_of_words = description.split()
+
+  feelings_list = []
+  encouragement_list = []
+  counter = 0
+  
+  for each_word in list_of_words:
+    
+    if each_word == "sad":
+      feelings_list.append("sad")
+      encouragement_list.append("Get over ur worries and tmrw will be better! ")
+      counter += 1
+    if each_word == "happy":
+      feelings_list.append("happy")
+      encouragement_list.append("That's great!")
+      counter += 1
+    if each_word == "tired":
+      feelings_list.append("tired")
+      encouragement_list.append("Take a break!")
+      counter += 1
+      
+    if each_word == "angry
+      feelings_list.append("angry")
+      encouragement_list.append("Calm down, don't act rash!")
+      counter += 1
+      
+    if each_word == "annoyed
+      feelings_list.append("annoyed")
+      encouragement_list.append("Cool down... Distract yourself from the annoyance!")
+      counter += 1
+      
+    if each_word == "troubled"
+      feelings_list.append("troubled")
+      encouragement_list.append("
+      counter += 1
+    
+      
+
+  if counter == 0:
+    
+      output = "Do you want to share with me more details?"
+
+  elif counter == 1:
+             
+      output = "It seems that you are feeling quite " + feelings_list[0] + ". However, do remember that "+ encouragement_list[0] + "! Hope you feel better :)"  
+
+  else:
+
+    feelings = ""    
+    for i in range(len(feelings_list)-1):
+      feelings += feelings_list[i] + ", "
+    feelings += "and " + feelings_list[-1]
+    
+    encouragement = ""    
+    for j in range(len(encouragement_list)-1):
+      encouragement += encouragement_list[i] + ", "
+    encouragement += "and " + encouragement_list[-1]
+
+    output = "It seems that you are feeling quite " + feelings + ". Please always remember "+ encouragement + "! Hope you feel better :)"
+
+  print()
+  print(output)
+  print()
